1b51c76ee161793c95e2ae1de8caeb8b92387583
joel <joelhooks@gmail.com>
2025-06-02 10:12:00 -0700

    update: project setup

--- env-check.ts
+++ env-check.ts
@@ -1,8 +1,9 @@
-import "dotenv/config";
+import dotenvFlow from 'dotenv-flow';
+dotenvFlow.config();
 
 function main() {
   console.log("Hello, Conf!");
-  console.log(process.env.OPENAI_API_KEY);
+  console.log(process.env.VERCEL_OIDC_TOKEN);
 }
 
 main();
